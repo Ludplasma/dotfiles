e1edbc00418d9a19ee57d3f33746f3933179a25b
Ludplasma <lightinghacker207@gmail.com>
2026-03-07 17:34:09 +0200

    changes

--- flatpak_install.sh
+++ flatpak_install.sh
@@ -11,3 +11,6 @@ flatpak install flathub org.vinegarhq.Sober
 curl https://launcher.hytale.com/builds/release/linux/amd64/hytale-launcher-latest.flatpak --output ~/.cache/hytale-launcher-latest.flatpak
 flatpak install ~/.cache/hytale-launcher-latest.flatpak
 rm ~/.cache/hytale-launcher-latest.flatpak
+
+curl https://github.com/ppy/osu/releases/latest/download/osu.AppImage --output ~/Games/osu.AppImage
+sudo chmod +x ~/Games/osu.AppImage
